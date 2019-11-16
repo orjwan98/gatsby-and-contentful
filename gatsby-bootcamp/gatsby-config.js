38aa56c992d51d0f3516dbdb8a8a74c587428919
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-11-16 19:38:49 +0200

    setup environment variables for contentful space

--- gatsby-bootcamp/gatsby-config.js
+++ gatsby-bootcamp/gatsby-config.js
@@ -4,12 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'fullstack bootcamp', 
     author: 'Orjwan Rajaby'
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful', 
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID, 
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     'gatsby-plugin-sass', 
     {
       resolve: 'gatsby-source-filesystem', 
